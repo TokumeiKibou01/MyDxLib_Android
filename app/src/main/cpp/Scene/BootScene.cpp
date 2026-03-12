35e217d06e7ef19b032aec835f5bf9c49a91eb13
TokumeiKibou01 <snowsansub011307@gmail.com>
2026-03-13 00:43:00 +0900

    ＜追加＞　ImGUIウインドウに条件コンパイルを追加

--- app/src/main/cpp/Scene/BootScene.cpp
+++ app/src/main/cpp/Scene/BootScene.cpp
@@ -14,11 +14,13 @@ void BootScene::Draw() {
     DrawString(0, 0, "Drawing BootScene", GetColor(255, 255, 255));
     DrawFormatString(0, 100, GetColor(255, 255, 255), "Counter: %d", sceneCounter_);
 
+#ifdef DEBUG_MODE
     ImGui::Begin("Debug");
     ImGui::Text("Counter: %d", sceneCounter_);
     ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
     ImGui::Text("Mouse Position: %f, %f", ImGui::GetMousePos().x, ImGui::GetMousePos().y);
     ImGui::End();
+#endif
 }
 
 void BootScene::Init() {
